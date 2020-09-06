74443ff4358d54232a9744c825b7d4355b191107
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-09-06 22:00:27 +0800

    a

--- js/cute.js
+++ js/cute.js
@@ -6,6 +6,10 @@ function aboutpage(){
     window.location.replace("../about.html");
 }
 
+function statspage(){
+    window.location.replace("../stats.html");
+}
+
 function getHTML(id){
     return document.getElementById(id)
 }
@@ -14,6 +18,7 @@ function getHTML(id){
 document.addEventListener('DOMContentLoaded', function () {
     getHTML("nav-home").addEventListener('click', homepage);
     getHTML("nav-about").addEventListener('click', aboutpage);
+    getHTML("nav-stats").addEventListener('click', statspage);
 
 
 });
